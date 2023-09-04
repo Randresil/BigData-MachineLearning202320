aff134f65756aa7882fc916fd98450cf05737d92
Ricardo Andrés Silva Torres <ra.silva@uniandes.edu.co>
2023-09-04 12:52:33 -0500

    Update Taller 1.R

--- Taller 1/Scripts/Taller 1.R
+++ Taller 1/Scripts/Taller 1.R
@@ -63,7 +63,5 @@ format(object.size(df_final), units = "MB")
 write_csv(df_final, "/Users/ricardoandressilvatorres/Documents/GitHub/
                       BigData-MachineLearning202320/Taller 1/stores/df_final.csv")
 
-
-
-
+### FIN DE WEB - SCRAPING ###
 
